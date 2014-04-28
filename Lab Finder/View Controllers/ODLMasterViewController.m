ee9528bd68b73e823a00e648939467015aabe526
Daniel Tomlinson <Dan.Tomlinson@wearebase.com>
2014-04-28 10:28:15 +0100

    Create ODLMasterViewController

---
+++ Lab Finder/View Controllers/ODLMasterViewController.m
@@ -0,0 +1,38 @@
+//
+//  ODLMasterViewController.m
+//  Lab Finder
+//
+//  Created by Daniel Tomlinson on 28/04/2014.
+//  Copyright (c) 2014 We Are Base. All rights reserved.
+//
+
+#import "ODLMasterViewController.h"
+
+@interface ODLMasterViewController ()
+
+@end
+
+@implementation ODLMasterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        // Custom initialization
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
