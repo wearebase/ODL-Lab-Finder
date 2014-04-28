878f78938a2402af315374cc6efd0ade979216d0
Daniel Tomlinson <Dan.Tomlinson@wearebase.com>
2014-04-28 14:04:13 +0100

    Display address

---
+++ Lab Finder/Views/ODLAddressTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ODLAddressTableViewCell.h
+//  Lab Finder
+//
+//  Created by Daniel Tomlinson on 28/04/2014.
+//  Copyright (c) 2014 We Are Base. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODLAddressTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UITextView *addressTextView;
+@end
---
+++ Lab Finder/Views/ODLAddressTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  ODLAddressTableViewCell.m
+//  Lab Finder
+//
+//  Created by Daniel Tomlinson on 28/04/2014.
+//  Copyright (c) 2014 We Are Base. All rights reserved.
+//
+
+#import "ODLAddressTableViewCell.h"
+
+@interface ODLAddressTableViewCell ()
+
+@end
+
+@implementation ODLAddressTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
